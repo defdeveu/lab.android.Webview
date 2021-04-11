dbd542edcc07aab079f1bb1437fca450e85c11e6
Füle Győző Ferenc <fgyozo@webeye.info>
2021-04-11 07:50:46 +0200

    first commit

---
+++ app/src/main/java/defdeveu/lab/android/webview/MainActivity.kt
@@ -0,0 +1,21 @@
+package defdeveu.lab.android.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        initWebView()
+    }
+
+    private fun initWebView(){
+        web_view.loadUrl("https://index.hu")
+    }
+}
